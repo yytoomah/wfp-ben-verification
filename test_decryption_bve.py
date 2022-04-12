33b6035b19f8194ac92cb6a3dd41e32085f06e8e
youssef.toomah <youssef.toomah@wfp.org>
2022-04-12 12:38:38 +0300

    replace batch file with command

--- test_decryption_bve.py
+++ test_decryption_bve.py
@@ -1,9 +1,8 @@
+from asyncio import subprocess
 from getpass import getpass
 from operator import truediv
 import os
 import sched
-from mysql.connector import connect, Error
-import mysql.connector
 import schedule, time
 from datetime import datetime
 from sys import exit
@@ -11,82 +10,71 @@ import boto3
 import yaml
 import gc
 import pandas
+import subprocess
 
-def get_params():
-    get_input_params = "SELECT * FROM credentials"
-    try:
-        with connect(
-            host='127.0.0.1',
-            user='root',
-            password='Mysql@2022',
-            database='bve_input_args'
-        ) as connection:
-            print(connection)
-            with connection.cursor() as cursor:
-                cursor.execute(get_input_params)
-                result = cursor.fetchall()
-                for row in result:
-                    inputs = row
-                    print(row)     
-    except Error as e:
-        print(e)
-    # print(inputs)
-
-    path_to_briefcase = inputs[2]
-    #print(path_to_briefcase)
-    username = inputs[0]
-    #print(username)
-    password = inputs[1]
-    #print(password)
-    storage_path = inputs[3]
-    #print(storage_path)
-    export_path = inputs[5]
-    #print(export_path)
-    form_id = inputs[4]
-    #print(form_id)
-    path_to_private_key = inputs[6]
-    #print(path_to_private_key)
-    project_url =  inputs[7]
-    #print(project_url)
-    access_key =  inputs[9]
-    secret_access_key =  inputs[10]
-
-    return inputs
 
 def export_data(config):
     print('start')
-    os.system("C:\\Users\\youssef.toomah\\Documents\\test_data_export.bat " 
-                + config['project_url'] + " "
-                + config['path_to_briefcase'] + " "               
-                + config['username'] + " "
-                + config['password'] + " "
-                + config['storage_path'] + " "
-                + config['form_id'] + " "
-                + config['export_path'] + " "
-                + datetime.now().strftime("%Y%m%d%H%M%S") + " "
-                + config['path_to_private_key'] + " "
-                + datetime.now().strftime("%Y-%m-%d"))
-                
+    
+    pro = ['java', '-jar', 
+                    config['path_to_briefcase'], 
+                    r'--pull_aggregate', 
+                    r'--start_from_last', 
+                    r'--odk_url',config['project_url'],
+                    r'--odk_username',config['username'],
+                    r'--odk_password',config['password'], 
+                    r'--storage_directory',config['storage_path'],
+                    r'--form_id',config['form_id']]
+
+    print(pro)
+    print('start pulling')
+    res = subprocess.call(pro)
+    print('submisisons pulled successfully')
+    print(res)
+
+    pro = ['java', '-jar', 
+                    config['path_to_briefcase'], 
+                    r'-e', 
+                    r'-oc', 
+                    r'-id',config['form_id'] ,
+                    r'-sd',config['storage_path'],
+                    r'-ed',config['export_path'], 
+                    r'-f',datetime.now().strftime("%Y%m%d%H%M%S"),
+                    r'-pf',config['path_to_private_key'],
+                    r'-start',datetime.now().strftime("%Y-%m-%d")]
+
+    print(pro)
+    print('start exporting')
+    res = subprocess.call(pro)
+    print('submisisons exported to {export_path} successfully'.format(export_path=config['export_path']))
+    print(res)
+
+    print('save file in NEST')
     write_csv_to_s3(config)
+    
     
 
 def write_csv_to_s3(config):
+    # create s3 connection
     s3_resource = boto3.resource('s3',aws_access_key_id=config['access_key'],aws_secret_access_key=config['secret_access_key'])
-    for file in os.listdir('C:\\1\\export'):
-        if file.endswith('.csv') and str.startswith(file,datetime.now().strftime("%Y%m%d")) and len(pandas.read_csv('C:\\1\\export\\' + file)) > 1:            
-             path_to_export_file = 'C:\\1\\export\\' + file
+    # loop over all files in the specified directory
+    for file in os.listdir(config['export_path']):
+        # test if the file is csv & is created today & contains submissions
+        if file.endswith('.csv') and str.startswith(file,datetime.now().strftime("%Y%m%d")) and len(pandas.read_csv(config['export_path']+ "\\"+ file)) > 1:            
+             path_to_export_file = '{export_path}\t{file}'.format(export_path=config['export_path'],file=file) 
              print(path_to_export_file)
              s3_resource.Object('389714470976-wfpnest-staging-dattestout',file).upload_file(path_to_export_file)
+             print('file successfully saved')
              os.remove(path_to_export_file)
         else:
-            print('not submissions file')
+            print('File could not be exported')
     print('end')
     print(datetime.now())
 
 with open('C:\\Users\\youssef.toomah\\Documents\\config.yaml') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 s = sched.scheduler(time.time, time.sleep)
-schedule.every().day.at("17:33").do(export_data,config)
+schedule.every().day.at("12:32").do(export_data,config)
 
 while True:
     schedule.run_pending()
